251ccea1a7e3da1b5b03429a2deac2a097759b94
LimaTarcisio <tarcisio.pesquisa.estudo@gmail.com>
2025-01-06 19:27:16 -0300

    código que automatiza o preenchimento de formulários, retirando os dados de planilhas em formato csv, concluido

--- planilhas.py
+++ planilhas.py
@@ -2,69 +2,60 @@ import pyautogui as pc
 import time as tempo
 
 pc.press("win")
-pc.write("chrome")
+tempo.sleep(2)
+pc.write("Edge")
+tempo.sleep(2)
 pc.press("enter")
-
+tempo.sleep(5)
 # entrar no link 
-pc.write("local")
+pc.write("localhost:820/formulario_s_banco/index.html")
 pc.press("enter")
-tempo.sleep(3)
+tempo.sleep(5)
+
+import pandas as pd # type: ignore
 
 
-
-pyautogui.press("win")
-pyautogui.write("chrome")
-pyautogui.press("enter")
-
-# entrar no link 
-pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
-pyautogui.press("enter")
-time.sleep(3)
-
-
-# Passo 2: Fazer login
-# selecionar o campo de email
-pyautogui.click(x=685, y=451)
-# escrever o seu email
-pyautogui.write("pythonimpressionador@gmail.com")
-pyautogui.press("tab") # passando pro próximo campo
-pyautogui.write("sua senha")
-pyautogui.click(x=955, y=638) # clique no botao de login
-time.sleep(3)
-
-# Passo 3: Importar a base de produtos pra cadastrar
-import pandas as pd
-
-tabela = pd.read_csv("produtos.csv")
+tabela = pd.read_csv("C:/Users/tarci/OneDrive/Área de Trabalho/Projetos-Git/projetos_em_python/dados_de_exemplo/planilhas/dados_teste.csv")
 
 print(tabela)
 
-# Passo 4: Cadastrar um produto
 for linha in tabela.index:
-    # clicar no campo de código
-    pyautogui.click(x=653, y=294)
-    # pegar da tabela o valor do campo que a gente quer preencher
-    codigo = tabela.loc[linha, "codigo"]
-    # preencher o campo
-    pyautogui.write(str(codigo))
-    # passar para o proximo campo
-    pyautogui.press("tab")
-    # preencher o campo
-    pyautogui.write(str(tabela.loc[linha, "marca"]))
-    pyautogui.press("tab")
-    pyautogui.write(str(tabela.loc[linha, "tipo"]))
-    pyautogui.press("tab")
-    pyautogui.write(str(tabela.loc[linha, "categoria"]))
-    pyautogui.press("tab")
-    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
-    pyautogui.press("tab")
-    pyautogui.write(str(tabela.loc[linha, "custo"]))
-    pyautogui.press("tab")
-    obs = tabela.loc[linha, "obs"]
-    if not pd.isna(obs):
-        pyautogui.write(str(tabela.loc[linha, "obs"]))
-    pyautogui.press("tab")
-    pyautogui.press("enter") # cadastra o produto (botao enviar)
-    # dar scroll de tudo pra cima
-    pyautogui.scroll(5000)
-    # Passo 5: Repetir o processo de cadastro até o fim
+    #pc.click(x=653, y=294)
+    pc.press("tab")
+    pc.write(str(tabela.loc[linha, "Nome"]))
+    pc.press("tab")
+
+    dado_empresa = tabela.loc[linha, "Empresa"]
+    if not pd.isna(dado_empresa):
+        pc.write(str(dado_empresa))
+    pc.press("tab")
+
+    pc.write(str(tabela.loc[linha, "Cidade"]))
+    pc.press("tab")
+    pc.write(str(tabela.loc[linha, "Estado"]))
+    pc.press("tab")
+    pc.write(str(tabela.loc[linha, "Endereço"]))
+    pc.press("tab")
+    pc.write(str(tabela.loc[linha, "Nro"]))
+    pc.press("tab")
+
+    dado_complemento = tabela.loc[linha, "Complemento"]
+    if not pd.isna(dado_complemento):
+        pc.write(str(dado_complemento))
+    pc.press("tab")
+
+    dado_bairro = tabela.loc[linha, "Bairro"]
+    if not pd.isna(dado_bairro):
+        pc.write(str(dado_bairro))
+    pc.press("tab")
+
+    dado_cep = tabela.loc[linha, "Bairro"]
+    if not pd.isna(dado_cep):
+        pc.write(str(dado_cep))
+    pc.press("tab")
+
+    pc.write(str(tabela.loc[linha, "Pais"]))
+    pc.press("tab")
+
+    pc.press("enter")
+    pc.scroll(5000)
